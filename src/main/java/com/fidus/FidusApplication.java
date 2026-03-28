d2e04626bf317ec37846dbcc083c8952bbb3346a
GiorgiCL <s054865@ad.viko.lt>
2026-03-28 14:43:40 +0200

    Feat: Initial phase

---
+++ src/main/java/com/fidus/FidusApplication.java
@@ -0,0 +1,13 @@
+package com.fidus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FidusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fidus/FidusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fidus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
